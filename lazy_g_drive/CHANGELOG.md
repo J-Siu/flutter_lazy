4c2d88a4ef9ba80b5dd2ba6f05fbb7b88967de2d
John, Sing Dao, Siu <john.sd.siu@gmail.com>
2022-08-27 20:21:30 -0400

    lazy_g_drive 1.0.1

--- lazy_g_drive/CHANGELOG.md
+++ lazy_g_drive/CHANGELOG.md
@@ -1,3 +1,4 @@
+## 1.0.1
+- [GDriveStringExt] [toMedia] use [toUtf8] instead of [codeUnit] for conversion.
 ## 1.0.0
-
 - Initial version.
--- lazy_g_drive/lib/src/extension.dart
+++ lazy_g_drive/lib/src/extension.dart
@@ -1,8 +1,9 @@
 import 'package:googleapis/drive/v3.dart' as gd;
+import 'package:lazy_extensions/lazy_extensions.dart' as lazy;
 
 /// ### Lazy extension for [String]
 extension GDriveStringExt on String {
   /// Create [Media] from [this] String
   gd.Media toMedia() =>
-      gd.Media(Future.value(codeUnits).asStream().asBroadcastStream(), length);
+      gd.Media(Future.value(toUtf8()).asStream().asBroadcastStream(), length);
 }
